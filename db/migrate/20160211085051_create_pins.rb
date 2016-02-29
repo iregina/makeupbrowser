2e120084f03d70006f6f5153c8a6fa246f03be34
iregina <reginawong14@gmail.com>
2016-02-28 22:43:26 -0800

    change pins to images

--- db/migrate/20160211085051_create_pins.rb
+++ db/migrate/20160211085051_create_pins.rb
@@ -1,8 +1,9 @@
-class CreatePins < ActiveRecord::Migration
+class CreateImages < ActiveRecord::Migration
   def change
-    create_table :pins do |t|
-      t.string :description
-
+    create_table :images do |t|
+      t.text :description
+      t.belongs_to :user, index: true
+      t.belongs_to :occasion_id, index: true
       t.timestamps null: false
     end
   end
