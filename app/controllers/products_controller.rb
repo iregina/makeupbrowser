27a39d3047f8453e60bb88431b95a266cab8f028
iregina <reginawong14@gmail.com>
2016-03-05 23:59:42 -0800

    yesterday changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,32 @@
 class ProductsController < ApplicationController
 	before_action :set_image, only: [:create, :edit, :update, :destroy]
 	def new
-		
+		@product = Product.new
 	end
 
 	def create
-		@product = Product.find_or_create_by(product_params)
+		@product = Product.new(product_params)
+		
 	  if @product.save
-	    @product.image = @image
-	    redirect_to product_path(@product)
+	  	p "*" * 40
+	    p params
+	    redirect_to image_path
 	  else
 	    render action: :new
 	  end
 	end
 
 	private
+
+
     # Use callbacks to share common setup or constraints between actions.
     def set_image
       @image = Image.find_by(id: params[:id])
+    end
+
+
+    def product_params
+      params.require(:product).permit(:name)
     end
 
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,7 @@ class Image < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :occasion
 	has_and_belongs_to_many :products
+
 	has_attached_file :image, 
 				# :url => "/public/uploads/:style/:filename",
 				# :path => ":rails_root/public/uploads/:style/:filename",
