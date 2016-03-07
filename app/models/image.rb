099848ca9fc376c7b423c789dacd8e98be07efb2
iregina <reginawong14@gmail.com>
2016-03-06 20:51:24 -0800

    make index page photos square

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,7 +6,7 @@ class Image < ActiveRecord::Base
 	has_attached_file :image, 
 				# :url => "/public/uploads/:style/:filename",
 				# :path => ":rails_root/public/uploads/:style/:filename",
-				:styles => { :medium => "300x300>", :thumb => "100x100>" }
+				:styles => { :full => "300x300>", :medium => "300x300#", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 	# validates :image, presence: true
 	# validates :description, presence: true
