fd0ba6bb8684b3e6340dbe9073bc23dde26a74f0
iregina <reginawong14@gmail.com>
2016-03-06 16:17:55 -0800

    create form to let users add products to images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,22 @@
 class ProductsController < ApplicationController
-	before_action :set_image, only: [:create, :edit, :update, :destroy]
+	before_action :set_image, only: [:new, :create, :edit, :update, :destroy]
+	
 	def new
 		@product = Product.new
+		# @image = Image.find_by(id: params[:id])
+		# p "The Image is:"
+		# p @image
 	end
 
 	def create
 		@product = Product.new(product_params)
-		
+		@image = Image.find_or_create_by(params[:image])
+		p "HELLO KITTY" * 20
 	  if @product.save
+	  	@product.images << @image
 	  	p "*" * 40
-	    p params
-	    redirect_to image_path
+	    p params[:image_id]
+	    redirect_to @image
 	  else
 	    render action: :new
 	  end
@@ -28,5 +34,6 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:name)
     end
+
 
 end
